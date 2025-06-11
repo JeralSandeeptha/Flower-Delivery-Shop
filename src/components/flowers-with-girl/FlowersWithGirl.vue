71a7afb5f9df267b4cd6a8b02a841bd8c72f69ae
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-11 22:59:48 +0530

    Added Flowers with girl section

---
+++ src/components/flowers-with-girl/FlowersWithGirl.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import data from '../../constants/website.json';
+</script>
+    
+<template>
+    <div class="w-full border-[1px]">
+        <img 
+            :src="data.pages[0].sections[3].data[0].image_url" 
+            :alt="data.pages[0].sections[3].data[0].image_alt"
+            class="w-full min-h[100vh] object-cover"
+        >
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/subscription-page/SubscriptionPage.vue
+++ src/views/subscription-page/SubscriptionPage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import SubscriptionHero from '../../components/subscription-hero/SubscriptionHero.vue';
 import HowDoesItWork from '../../components/how-does-it-work/HowDoesItWork.vue';
+import FlowersWithGirl from '../../components/flowers-with-girl/FlowersWithGirl.vue';
 </script>
     
 <template>
     <SubscriptionHero />
     <HowDoesItWork />
+    <FlowersWithGirl />
 </template>
 
 <style scoped>
