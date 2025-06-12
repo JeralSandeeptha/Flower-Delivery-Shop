cc290593eb78e3949871926fd4ca16948db5daa1
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-12 05:41:06 +0530

    Added home-service section

---
+++ src/components/home-service/HomeService.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Header from '../../components/header/Header.vue';
+</script>
+    
+<template>
+    <div class="w-full min-h-[20vh] border-1 flex items-center justify-center">
+        <Header title="Our Service"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/home-page/HomePage.vue
+++ src/views/home-page/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import WhyChooseUs from '../../components/why-choose-us/WhyChooseUs.vue';
+import HomeService from '../../components/home-service/HomeService.vue';
 </script>
 
 <template>
     <WhyChooseUs />
+    <HomeService />
 </template>
 
 <style scoped>
