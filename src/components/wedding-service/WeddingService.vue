b78e77e890ca8f0a2f36d5a3bb788841f6048706
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-13 05:03:07 +0530

    Added wedding section

---
+++ src/components/wedding-service/WeddingService.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import WeddingService from '../wedding-service/WeddingService.vue';
+import Paragraph from '../paragraph/Paragraph.vue';
+import Header from '../header/Header.vue';
+import data from '../../constants/website.json';
+</script>
+    
+<template>
+    <div class="bg-image w-full min-h-[100vh] flex flex-col items-center justify-center text-center gap-[2rem] max-md:p-8">
+        <h4 class="text-[white] faq-text">{{ data.pages[2].sections[2].esction_name }}</h4>
+        <div class="flex flex-col gap-[1rem]">
+            <Header 
+                style="color: white !important"
+                :title="data.pages[2].sections[2].data[0].title"
+            />
+            <Paragraph 
+                class="text-white max-w-[500px]"
+                :description="data.pages[2].sections[2].data[0].description"
+            />
+        </div>
+        <router-link to="/contact">
+            <button class="btn-secondary">{{ data.pages[2].sections[2].data[0].btton_text }}</button>
+        </router-link>
+    </div>
+</template>
+
+<style scoped>
+.bg-image {
+    background-image: url('../../../public/images/wedding-bg.png');
+    background-size: cover;
+    background-position: center;
+}
+</style>
--- src/views/home-page/HomePage.vue
+++ src/views/home-page/HomePage.vue
@@ -2,12 +2,14 @@
 import WhyChooseUs from '../../components/why-choose-us/WhyChooseUs.vue';
 import HomeService from '../../components/home-service/HomeService.vue';
 import HomeOurService from '../../components/home-our-service/HomeOurService.vue';
+import WeddingService from '../../components/wedding-service/WeddingService.vue';
 </script>
 
 <template>
     <WhyChooseUs />
     <HomeService />
     <HomeOurService />
+    <WeddingService />
 </template>
 
 <style scoped>
