02732f32e59eb5690bb7efd7f6fe3c2211f6d7ae
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-17 07:13:21 +0530

    Added home hero

--- src/components/home-hero/HomeHero.vue
+++ src/components/home-hero/HomeHero.vue
@@ -1,26 +1,15 @@
 <script setup lang="ts">
-import data from '../../constants/website.json';
-import Header from "../header/Header.vue";
-import SubHeader from "../sub-header/SubHeader.vue";
 </script>
 
 <template>
-    <div class="w-full min-h-[calc(100vh-80px)] grid grid-cols-2 max-md:grid-cols-1">
-    <div
-      class="bg-image h-[calc(100vh-80px)] border-[1px] flex items-center justify-center"
-    >
-      <Header title="Fresh Flowers" :isLight="true" />
-    </div>
-    <div
-      class="grid grid-cols-2 min-h-[calc(100%-80px)] border-[1px] max-sm:grid-cols-1 "
-    >
-      <div v-for="section in data.pages[2].sections[4]">
-          
-      </div>
-    </div>
-  </div>
+  <div class="bg-image w-full min-h-[calc(100vh-80px)]"></div>
 </template>
 
 <style scoped>
-
+.bg-image {
+  background-image: url('https://plus.unsplash.com/premium_photo-1675063044882-522a7d281b2f?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
 </style>
