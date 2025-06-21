f7d82d524d705fca16549965a84f5e0ff03aaede
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-06-21 21:08:22 +0530

    Added login page

--- src/views/login-page/LoginPage.vue
+++ src/views/login-page/LoginPage.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import Button from '../../components/button/Button.vue';
+import Header from '../../components/header/Header.vue';
+import Paragraph from '../../components/paragraph/Paragraph.vue';
+
+const email = ref<string>('');
+const password = ref<string>('');
 
 </script>
 
 <template>
-    Login Page
+    <div class="w-full min-h-[calc(100vh-80px)] flex flex-col items-center justify-center p-18 max-sm:p-8 max-xs:p-5">
+        <div class="max-w-[500px] mx-auto w-[500px] max-lg:w-[100%] p-10 max-sm:p-8 max-xs:p-5 border-[2px]">
+            <div class="flex flex-col gap-[1rem] mb-[1rem]">
+                <Header title="Greetings! Welcome to Your Account."/>
+                <Paragraph description="Use your email address and password to log to your account"/>
+                <input type="email" name="email" v-model="email" placeholder="Email Address" class="py-3 outline-none border-2 border-gray-200 p-2 text-gray-500 text-sm w-full font-light">
+                <input type="email" name="email" v-model="password" placeholder="Password" class="py-3 outline-none border-2 border-gray-200 p-2 text-gray-500 text-sm w-full font-light">
+                <Button title="CONTINUE" :isFullWidth="true" class="bg-black text-white border-[black]"/>
+            </div>
+            <hr class="border-[1px] borde-[gray]">
+            <div class="flex flex-col gap-[1rem] mt-[1rem]">
+                <Paragraph description="Instantly, Log to your account via Google"/>
+                <Button title="CONTINUE WITH GOOGLE" :isFullWidth="true" class="bg-black text-white border-[black]"/>
+            </div>
+            <div class="mt-[1rem] flex gap-[1rem] items-center">
+                <Paragraph description="Don't have an account?"/>
+                <router-link to="/register">
+                    <Paragraph description="Create Account" class="underline"/>
+                </router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
--- src/views/register-page/RegisterPage.vue
+++ src/views/register-page/RegisterPage.vue
@@ -11,18 +11,24 @@ const password = ref<string>('');
 
 <template>
     <div class="w-full min-h-[calc(100vh-80px)] flex flex-col items-center justify-center p-18 max-sm:p-8 max-xs:p-5">
-        <div class="max-w-[500px] mx-auto w-[500px] p-10 max-sm:p-8 max-xs:p-5 border-[2px]">
+        <div class="max-w-[500px] mx-auto w-[500px] max-lg:w-[100%] p-10 max-sm:p-8 max-xs:p-5 border-[2px]">
             <div class="flex flex-col gap-[1rem] mb-[1rem]">
-                <Header title="Greetings! Welcome to luxury gift shop."/>
-                <Paragraph description="Use your email address and password to sign up or log in"/>
+                <Header title="Greetings! Create Your New Account."/>
+                <Paragraph description="Use your email address and password to create new account"/>
                 <input type="email" name="email" v-model="email" placeholder="Email Address" class="py-3 outline-none border-2 border-gray-200 p-2 text-gray-500 text-sm w-full font-light">
                 <input type="email" name="email" v-model="password" placeholder="Password" class="py-3 outline-none border-2 border-gray-200 p-2 text-gray-500 text-sm w-full font-light">
-                <Button title="CONTINUE" :isFullWidth="true" class="bg-black text-white border-[black]"/>
+                <Button title="CREATE ACCOUNT" :isFullWidth="true" class="bg-black text-white border-[black]"/>
             </div>
             <hr class="border-[1px] borde-[gray]">
             <div class="flex flex-col gap-[1rem] mt-[1rem]">
-                <Paragraph description="Instantly login or sign up via Google"/>
+                <Paragraph description="Instantly, create new account via Google"/>
                 <Button title="CONTINUE WITH GOOGLE" :isFullWidth="true" class="bg-black text-white border-[black]"/>
+            </div>
+            <div class="mt-[1rem] flex gap-[1rem] items-center">
+                <Paragraph description="Already have an account?"/>
+                <router-link to="/login">
+                    <Paragraph description="Log In" class="underline"/>
+                </router-link>
             </div>
         </div>
     </div>
